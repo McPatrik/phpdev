07c14dac1be9f42bf28f3a0db9701d9a93a65120
Tatiana <you@example.com>
2022-01-26 17:29:10 +0200

    .

---
+++ phpdev-init.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo apt install curl git php-fpm nginx mysql-server phpmyadmin memcached redis-server php-redis php-memcached
+sudo mysql -uroot
+sudo snap install --classic code
+mysql < i.sql
+sudo systemctl restart mysql.service
