a7357280c90acd7488320d74c2f293fb4aacf8d7
McPatrik <44431020+McPatrik@users.noreply.github.com>
2022-01-26 17:37:55 +0200

    Update phpdev-init.sh

--- phpdev-init.sh
+++ phpdev-init.sh
@@ -3,5 +3,5 @@
 sudo apt install curl git php-fpm nginx mysql-server phpmyadmin memcached redis-server php-redis php-memcached
 sudo mysql -uroot
 sudo snap install --classic code
-mysql < i.sql
+#mysql > source i.sql
 sudo systemctl restart mysql.service
